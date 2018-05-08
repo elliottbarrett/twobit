0df779fd1eedda34f1672c2ce99ce70ecb158be9
Elliott Barrett <elliottjbarrett@gmail.com>
2018-05-08 16:22:13 -0300

    Palette window (for level editing) comes up, loads. Tracking of tile number works. This is largely tied to the existing level tileset.

--- src/main.cpp
+++ src/main.cpp
@@ -17,9 +17,39 @@
 
 int main()
 {
+    // Main window
     sf::RenderWindow window(sf::VideoMode(GB_WIDTH * WINDOW_SCALE, GB_HEIGHT * WINDOW_SCALE), "SFML works!");
-    sf::Clock frameTimeClock;
     window.setVerticalSyncEnabled(true);
+
+    // Editor palette
+    sf::Texture paletteTexture;
+    sf::Sprite paletteSprite;
+    sf::Vector2u paletteSize;
+    sf::View paletteView;
+    int paletteTileNumber = 0;
+
+    paletteTexture.loadFromFile("assets/LevelTileset.png");
+    paletteSize = paletteTexture.getSize();
+    paletteSprite.setTexture(paletteTexture);
+
+    sf::RectangleShape paletteSelectionHighlight(sf::Vector2f(16,16));
+    paletteSelectionHighlight.setFillColor(sf::Color::Transparent);
+    paletteSelectionHighlight.setOutlineThickness(1);
+    paletteSelectionHighlight.setOutlineColor(sf::Color(250, 150, 100));
+
+    sf::RenderWindow paletteWindow(sf::VideoMode(paletteSize.x * 2, paletteSize.y * 2), "Editor Palette");
+    paletteWindow.setSize(sf::Vector2u(2 * paletteSize.x, 2 * paletteSize.y));
+    paletteWindow.setView(sf::View(sf::FloatRect(0, 0, (float) paletteTexture.getSize().x, (float) paletteTexture.getSize().y)));
+    paletteWindow.setPosition(sf::Vector2i(900, 0));
+    paletteWindow.setVerticalSyncEnabled(true);
+
+    paletteView = paletteWindow.getDefaultView();
+    paletteView.setCenter(paletteSize.x / 2, paletteSize.y / 2);
+    paletteView.zoom(0.5);
+    paletteWindow.setView(paletteView);
+
+    // Timer
+    sf::Clock frameTimeClock;
 
     // Fixing the fucking coordinate system...
     sf::View yIsUpView = window.getDefaultView();
@@ -48,7 +78,6 @@ int main()
         return -1;
     }
 
-
     while (window.isOpen())
     {
         sf::Event event;
@@ -74,6 +103,32 @@ int main()
         window.draw(tileMap);
         window.draw(testSprite);
         window.display();
+
+        while (paletteWindow.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                paletteWindow.close();
+
+            if (event.type == sf::Event::MouseButtonPressed)
+            {
+                sf::Vector2f worldPos = paletteWindow.mapPixelToCoords(sf::Mouse::getPosition(paletteWindow));
+                int paletteX = (int) worldPos.x;
+                int paletteY = (int) worldPos.y;
+
+                int tileX = paletteX / 16;
+                int tileY = paletteY / 16;
+
+                paletteSelectionHighlight.setPosition(16 * tileX, 16 * tileY);
+                paletteTileNumber = (tileY * (paletteSize.x / 16)) + tileX;
+
+                std::cout << paletteTileNumber << std::endl;
+            }
+        }
+
+        paletteWindow.clear();
+        paletteWindow.draw(paletteSprite);
+        paletteWindow.draw(paletteSelectionHighlight);
+        paletteWindow.display();
     }
 
     return 0;
