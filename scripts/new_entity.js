c3394478a156f4ff0c161ea2366189e656613360
Elliott Barrett <elliottjbarrett@gmail.com>
2018-06-14 22:52:20 -0300

    Added new_entity script --- guess I had the boilerplate blues

---
+++ scripts/new_entity.js
@@ -0,0 +1,103 @@
+'use strict'
+
+const fs = require('fs');
+const path = require('path');
+
+function parseArgs() {
+    const params = {};
+    const args = process.argv;
+
+    if (args.length < 3) {
+        // Handle failure case
+    }
+
+    params.entityName = args[2];
+    params.headerGuard = args[2].toUpperCase() + "_H_INCLUDED";
+
+    return params;
+}
+
+function makeHeader(args) {
+    const headerFile = 
+`#ifndef ${args.headerGuard}
+#define ${args.headerGuard}
+
+#include "Entity.h"
+
+class ${args.entityName} : public Entity
+{
+public:
+    ${args.entityName}(unsigned int id, std::string name, std::vector<std::string> params);
+    ~${args.entityName}();
+
+    void update(float dt);
+    std::string getEntityDescription();
+    EntityType getEntityType();
+    void handleEntityCollision(Entity* other);
+
+private:
+    void initParameters(std::vector<std::string> params);
+};
+
+#endif
+`;
+
+    return headerFile;
+}
+
+function makeCpp(args) {
+    const cppFile =
+`#include "${args.entityName}.h"
+
+${args.entityName}::${args.entityName}(unsigned int id, std::string name, std::vector<std::string> params) :
+    Entity(id, name, params)
+{
+
+}
+
+${args.entityName}::~${args.entityName}()
+{
+
+}
+
+void ${args.entityName}::initParameters(std::vector<std::string> params)
+{
+
+}
+
+EntityType ${args.entityName}::getEntityType()
+{
+    //fixme;
+}
+
+std::string ${args.entityName}::getEntityDescription()
+{
+    return std::to_string(id) + " ${args.entityName} " + name + "\\n"
+        + getCommonParameters();
+}
+
+void ${args.entityName}::update(float dt)
+{
+    Entity::update(dt);
+}
+
+void ${args.entityName}::handleEntityCollision(Entity *other)
+{
+    switch (other->getEntityType())
+    {
+    default:
+        break;
+    }
+}
+`;
+    return cppFile;
+}
+
+(() => {
+    const args = parseArgs();
+    const header = makeHeader(args);
+    const cpp = makeCpp(args);
+
+    fs.writeFileSync(path.join(__dirname, "..", "src", `${args.entityName}.h`), header);
+    fs.writeFileSync(path.join(__dirname, "..", "src", `${args.entityName}.cpp`), cpp);
+})();
